bd07a9098b0c21d50c896c757dcc47a769e2caca
Elizabeth Armstrong <earmstrong0730@gmail.com>
2019-10-15 10:49:46 -0700

    Deleted default splash screen and created a new one

--- Countr/Countr.Droid/obj/Debug/90/android/environment.arm64-v8a.s
+++ Countr/Countr.Droid/obj/Debug/90/android/environment.arm64-v8a.s
@@ -14,17 +14,15 @@ application_config:
 	.byte	0
 	/* uses_mono_aot */
 	.byte	0
-	/* uses_embedded_dsos */
-	.byte	0
 	/* uses_assembly_preload */
 	.byte	1
 	/* is_a_bundled_app */
 	.byte	0
 	/* environment_variable_count */
-	.zero	3
 	.word	10
 	/* system_property_count */
 	.word	0
+	.zero	4
 	/* android_package_name */
 	.xword	.L.str.1
 	.size	application_config, 24
@@ -65,7 +63,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"b7158c74-ada1-4a81-bff3-4e7bf1922874"
+	.asciz	"bd7f6f9c-aacc-4b5e-8601-ab03cb7da262"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
